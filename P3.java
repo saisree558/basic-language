b649d0c68e8888035f0a91f745a5af090afac7d9
saisree558 <58467471+saisree558@users.noreply.github.com>
2019-12-04 07:59:08 +0530

    Add files via upload

---
+++ P3.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+public class P3
+{
+public static void main(String args[])
+{
+int a = Integer.parseInt(args[0]);
+int b = Integer.parseInt(args[1]);
+System.out.println("The sum of "+args[0]+" and "+args[1]+" is " args[0]+args[1]);
+}
+}
